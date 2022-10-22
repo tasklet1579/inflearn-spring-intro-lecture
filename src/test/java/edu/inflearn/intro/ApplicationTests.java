551e839318f89a1f5afe230769016d479df13daa
tasklet1579 <tasklet1579@gmail.com>
2022-10-22 16:30:46 +0900

    프로젝트 생성

---
+++ src/test/java/edu/inflearn/intro/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.inflearn.intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
