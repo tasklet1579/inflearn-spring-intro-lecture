689a436bd68d4890413128e263b54ed4fa7a41a9
tasklet1579 <tasklet1579@gmail.com>
2022-10-23 11:35:36 +0900

    회원 서비스 개발

---
+++ src/main/java/edu/inflearn/intro/service/MemberService.java
@@ -0,0 +1,34 @@
+package edu.inflearn.intro.service;
+
+import edu.inflearn.intro.domain.Member;
+import edu.inflearn.intro.repository.MemberRepository;
+import edu.inflearn.intro.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository repository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+
+        repository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        repository.findByName(member.getName())
+                  .ifPresent(m -> {
+                      throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+                  });
+    }
+
+    public List<Member> findMembers() {
+        return repository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return repository.findById(memberId);
+    }
+}
